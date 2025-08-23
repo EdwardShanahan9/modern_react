d3183c6335a04057999dfaf68eb22824394d18bb
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-08-23 15:50:37 +0100

    Iimplement star rating functionality in Rating component

--- 02_React_Fundamentals/src/components/Rating.jsx
+++ 02_React_Fundamentals/src/components/Rating.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
   const stars = Array.from({ length: 5 }, (_, index) => index + 1);
+
   return (
     <div className="rating-container">
       <h2>Rate your experience</h2>
 
       <div className="stars">
         {stars.map((star) => (
-          <span key={star} className="star">
+          <span
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            onClick={() => setRating(star)}
+            key={star}
+            className="star"
+          >
             {`\u2605`}
           </span>
         ))}
