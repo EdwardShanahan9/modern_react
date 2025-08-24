9f3d3da47c1b3aa8ecb3838d03f763962a06bea0
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-08-24 13:17:43 +0100

    Add feedback message

--- 02_React_Fundamentals/src/components/Rating.jsx
+++ 02_React_Fundamentals/src/components/Rating.jsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 const Rating = () => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
-
   const stars = Array.from({ length: 5 }, (_, index) => index + 1);
+  const feedback = ["Terrible", "Poor", "Fair", "Good", "Excellent"];
 
   return (
     <div className="rating-container">
@@ -17,12 +17,14 @@ const Rating = () => {
             onMouseLeave={() => setHover(0)}
             onClick={() => setRating(star)}
             key={star}
-            className="star"
+            className={`star ${star <= (hover || rating) ? "active" : ""}`}
           >
             {`\u2605`}
           </span>
         ))}
       </div>
+
+      {rating > 0 && <p className="feedback">{feedback[rating - 1]}</p>}
     </div>
   );
 };
